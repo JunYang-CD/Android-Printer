11a85eaa754ee0acbe1d36f361b423a58d508361
Jerry Yang <jerry.yang3@anz.com>
2016-11-01 23:16:41 +0800

    Init demo app UI

--- app/src/main/java/jerry/demo/payment/payanywhere/MainActivity.java
+++ app/src/main/java/jerry/demo/payment/payanywhere/MainActivity.java
@@ -1,13 +1,21 @@
 package jerry.demo.payment.payanywhere;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import jerry.demo.payment.payanywhere.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    private MainActivityViewModel mainActivityViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mainActivityViewModel = new MainActivityViewModel();
+        mainActivityViewModel.headerText = getResources().getString(R.string.payment);
+        binding.setViewModel(mainActivityViewModel);
+
     }
 }
---
+++ app/src/main/java/jerry/demo/payment/payanywhere/MainActivityViewModel.java
@@ -0,0 +1,9 @@
+package jerry.demo.payment.payanywhere;
+
+/**
+ * Created by jerry on 11/1/16.
+ */
+
+public class MainActivityViewModel {
+    public String headerText;
+}
